1b6675b3d43d11ae57bad6583d4a143948166cdc
Daniel Pastushchak <danikpastushchak90@gmail.com>
2016-11-26 15:51:55 +0200

    fixes

--- photogallery/models.py
+++ photogallery/models.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-class Post(model.Model):  #определяем нашу модель
+
+class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
